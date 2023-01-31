be5b4b8bbde9f207dc1c540d79ec043afc6be01f
abigairi <abigailjonathan150@gmail.com>
2023-01-31 08:00:08 +0300

    learning operators

---
+++ week1.py
@@ -0,0 +1,63 @@
+my_list = [i for i in range (-1,2)]
+print(my_list)
+
+#qn 2
+Vals = [0,2,3]
+Vals.insert(0,1)
+del Vals[1]
+print(Vals)
+
+# qn 3
+my_list=[3,1,-2]
+print(my_list[my_list[-1]])
+
+#qn4
+a=1
+b=0
+c=a & b
+d= a| b
+e=a^b
+print(c + d + e)
+
+
+#qn 5
+nums=[1,2,3]
+Vals = nums [-1:-2]
+print(nums)
+
+
+#qn 5
+my_list=[1,2,3,4]
+
+print(my_list[-3:-2])
+
+#qn 6
+var=1
+while var < 10:
+            print("#")
+            var=var<< 1
+
+            #qn 7
+            """x=1
+            x=x==x
+            print(x)"""
+
+            #qn 8
+
+            nums=[1,2,3,]
+            Vals= nums
+            del Vals[1:2]
+            print(Vals)
+            print(nums)
+
+
+
+            for i in range (1):
+                        print("#")
+            else:
+                print("#")
+
+
+
+
+                
