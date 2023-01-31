1c9e7293bbf45475ec497b13516aeacd29268d68
abigairi <abigailjonathan150@gmail.com>
2023-01-31 09:14:19 +0300

    operators

---
+++ arthmetic.py
@@ -0,0 +1,36 @@
+p= 20
+f=400
+#additional operator
+sum = p + f
+print("Sum is equal to",sum)
+
+#subtracton operator
+sub1 = p - f
+sub2 = f - p
+print("Sub1 is equal to",sub1)
+print("Sub2 is equal to",sub2)
+
+#division
+div = p /f 
+div1 = p // f
+print("Div is equal to",div)
+print("Div1 is equal to",div1)
+
+#multiplication
+a =4
+b = 2
+mult1 = a * b
+mult2 = a ** b
+print("Mult1 is equal to",mult1)
+print("Mult2 is equal to",mult2)
+
+#modul operator
+
+module =a % b
+module2 = b % a
+val = a | b
+print(module)
+print(module2)
+print(val)
+
+
---
+++ comparison.py
@@ -0,0 +1,19 @@
+a = 22
+b =25
+if a > b:
+            print("a is greator than b")
+else:
+            print("a is not greator than b")
+
+            #comparison operator
+val =  b > a
+print(val)
+
+val1 = b < a
+print(val1)
+
+val2 = b == a
+print(val2)
+
+
+#logical operator
