4cdbacad8fd45fac6edc6564cf0a683a4cdf1ed9
abigairi <abigailjonathan150@gmail.com>
2023-01-31 20:18:51 +0300

    answers

--- comparison.py
+++ comparison.py
@@ -60,4 +60,69 @@ func=("asha","joe","loe")
 
 
 
+#maswali
 
+x = 10.0
+y = (x < 100.0) and isinstance(x, float)
+print(y)
+
+
+# maswali 01
+
+n = 1 + 2 ** 3 * 4
+print(n)
+
+#maswali 2
+
+x = 100
+x *= 5
+print(x)
+
+#maswali 3
+
+print(-18 // 4)
+
+# maswali 4
+a = [10, 20]
+b = a
+b += [30, 40]
+print(a)
+print(b)
+
+#maswali 5
+x = 100
+y = 50
+print(x and y)
+
+#maswali 6
+print(2%6)
+
+#maswali 7
+print(2 ** 3 ** 2)
+x = 6
+y = 2
+print(x ** y)
+print(x // y)
+
+#maswali 8
+a = 4
+b = 11
+print(a | b)
+print(a >> 2)
+
+#maswali 9
+print(10 - 4 * 2)
+
+#maswali 10
+print(2 * 3 ** 3 * 4)
+
+#maswali 11
+y = 10
+y += 2
+print(y)
+
+#maswali 12
+x = 10
+y = 50
+if x ** 2 > 100 and y < 100:
+    print(x, y)
