7afcda6326d19a2e14cdd5cd6ff8a7b9b5bcbea3
abigairi <abigailjonathan150@gmail.com>
2023-02-01 11:31:35 +0300

    lists

---
+++ lists.py
@@ -0,0 +1,15 @@
+aList = [1, 2, 3, 4, 5, 6, 7]
+pow2 =  [2 * x for x in aList]
+print(pow2)
+#answer 1
+aList = ["PYnative", [4, 8, 12, 16]]
+print(aList[0][1])    
+print(aList[1][3])
+
+#answer 2
+aList = [10, 20, 30, 40, 50, 60, 70, 80]
+print(aList[2:5])
+print(aList[:4])
+print(aList[3:])
+
+#answer 3
